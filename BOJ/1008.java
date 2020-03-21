d654506245ca082f7e3e76c01d1d0be777cf56fa
seth <seth.j.choi@gmail.com>
2020-03-21 20:17:38 +0900

    add #1008 using java

---
+++ BOJ/1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double a, b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        double multiplication = a / b;
+        System.out.print(multiplication);
+    }
+}
