388897630274bc82688e3e58eb7cc3aa194b0071
seth <seth.j.choi@gmail.com>
2020-05-16 23:24:11 +0900

    add #3053 using c++

---
+++ BOJ/3053.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double r;
+    double pi = 3.141592653589793238462643383279502884197169;
+    cin >> r;
+    cout.precision(6);
+    cout << fixed << pi * r * r << endl;
+    cout.precision(6);
+    cout << fixed << 2*r*r << endl;
+    return 0;
+}
