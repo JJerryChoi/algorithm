c2ba44f75bc08db44a79158100007689fcba6487
seth <seth.j.choi@gmail.com>
2020-06-10 19:33:01 +0900

    add #11654 using java

---
+++ BOJ/11654.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        int ascii = System.in.read();
+        System.out.print(ascii);
+    }
+}
