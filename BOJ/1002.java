e577c240ee4efa1b5b2d49e34115f92ca0327c22
JJerryChoi <jungil.j.choi@gmail.com>
2020-06-22 23:17:32 +0900

    delete variable pi

--- BOJ/1002.java
+++ BOJ/1002.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Main{
-    static final double pi = 3.14159265359;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
