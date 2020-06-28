2b1d7f3041aebe326a47b629bacaa557dea6a7bd
JJerryChoi <jungil.j.choi@gmail.com>
2020-06-28 19:54:51 +0900

    add #15653 using c++

---
+++ BOJ/15653.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> v;
+array<int, 8> output;
+array<bool, 8> check;
+
+void backTracking(int depth){
+    if(depth == m){
+        for(int i = 0; i < m-1; ++i)
+            if (output[i] > output[i + 1])
+                return;
+
+        for (int i = 0; i < m; ++i)
+            printf("%d ", output[i]);
+        printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < n; ++i){
+        check[i] = true;
+        output[depth] = v[i];
+        backTracking(depth + 1);
+        check[i] = false;
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    v.resize(n);
+    fill(check.begin(), check.end(), false);
+    for(int i = 0; i < n; ++i)
+        v[i] = i+1;
+    backTracking(0);
+    return 0;
+}
