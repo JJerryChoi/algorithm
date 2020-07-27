80db6e12bede74d615ac2d88fe3385edd3df5c87
JJerryChoi <jungil.j.choi@gmail.com>
2020-07-27 18:44:00 +0900

    add #5086 using c++

---
+++ BOJ/5086.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    while(true){
+        cin >> n >> m;
+        if(n == 0 && m == 0)
+            break;
+        if(m % n == 0)
+            cout << "factor\n";
+        else if(n % m == 0)
+            cout << "multiple\n";
+        else
+            cout << "neither\n";
+    }
+    return 0;
+}
