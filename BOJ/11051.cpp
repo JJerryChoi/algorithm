bb017b0333dbf9182205657d5ade0b8436f6cf38
JJerryChoi <jungil.j.choi@gmail.com>
2020-07-28 22:41:38 +0900

    delete unused function

--- BOJ/11051.cpp
+++ BOJ/11051.cpp
@@ -4,20 +4,6 @@ using namespace std;
 const int divisor = 10007;
 int binomialCoefficent[1001][1001];
 
-long long getBinomialCoefficent2(int n, int k){ // n! / k!(n-k)!
-    long long numerator = 1;
-    for(int i = k+1; i <= n; ++i) {
-        numerator *= i;
-    }
-
-    long long denominator = n-k;
-    for(int i = 2; i < n-k; ++i)
-        denominator *= i;
-    if(denominator == 0)
-        denominator = 1; // 0! = 1;
-    return numerator / denominator;
-}
-
 long long getBinomialCoefficent(int n, int k) {
     if(binomialCoefficent[n][k] != 0)
         return binomialCoefficent[n][k];
